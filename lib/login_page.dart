718ce5a3e2389a6d88dcae2adf08f0afb7bb665f
rdbudise0206 <fiveyearplan@gmail.com>
2025-01-09 12:31:18 +0700

    Update login_page.dart

---
+++ lib/login_page.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'admin_menu.dart';
+import 'user_menu.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isChecked = false;  // Untuk checkbox 'Remember Me'
+  String errorMessage = ''; // Menyimpan pesan error jika login gagal
+
+  Future<void> login() async {
+    final url = Uri.parse('https://tinoganteng.com/apii/api.php');
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'action': 'login',
+        'username': usernameController.text,
+        'password': passwordController.text,
+      }),
+      headers: {"Content-Type": "application/json"},
+    );
+
+    final responseData = json.decode(response.body);
+
+    if (responseData['success']) {
+      String userLevel = responseData['level'];
+      String username = responseData['username'];
+
+      // Convert id_login to string before storing
+      String idLogin = responseData['id_login'].toString();
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('username', username);
+      await prefs.setString('userLevel', userLevel);
+      await prefs.setString('id_login', idLogin);
+
+      if (userLevel == 'admin') {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => AdminMenu()),
+        );
+      } else if (userLevel == 'user') {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => UserMenu()),
+        );
+      }
+    } else {
+      setState(() {
+        errorMessage = 'Login failed! Please check your credentials.';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/login.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            // Background Image - Sudah disetting di Container di luar Scaffold
+
+            // Teks di atas Form
+            Positioned(
+              top: 100,
+              left: 35,
+              child: Text(
+                'Silahkan\nLogin',
+                style: TextStyle(color: Colors.white, fontSize: 33),
+              ),
+            ),
+
+            // Logo Global di bawah tulisan "Welcome Back" dan di atas form username
+            Positioned(
+              top: 300, // Posisi logo di bawah "Welcome Back"
+              left: 35, // Posisi logo di pojok kiri
+              child: Image.asset(
+                'assets/logo-global-institute-4.png',  // Gantilah dengan path logo Anda
+                width: 100,  // Menyesuaikan ukuran logo
+                height: 100, // Menyesuaikan ukuran logo
+              ),
+            ),
+
+            // Form Login
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 35, right: 35),
+                      child: Column(
+                        children: [
+                          // Username Field
+                          TextField(
+                            controller: usernameController,
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Username",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+
+                          // Password Field
+                          TextField(
+                            controller: passwordController,
+                            style: TextStyle(),
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Password",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+
+                          // Remember Me Checkbox
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Remember Me",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              Checkbox(
+                                value: isChecked,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isChecked = value!;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+
+                          // Login Button
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Sign in',
+                                style: TextStyle(
+                                  fontSize: 27,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Color(0xff4c505b),
+                                child: IconButton(
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    login();
+                                  },
+                                  icon: Icon(
+                                    Icons.arrow_forward,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          // Menampilkan pesan error jika login gagal
+                          if (errorMessage.isNotEmpty)
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20),
+                              child: Text(
+                                errorMessage,
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
